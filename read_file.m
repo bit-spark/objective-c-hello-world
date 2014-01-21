efbd717f38b660a6c811a110026aa0c3e8e52505
James Ridgway <james.ridgway@thefloow.com>
2014-01-21 08:41:09 +0000

    Refactor 2

--- read_file.m
+++ read_file.m
@@ -1,10 +1,7 @@
 #import <Foundation/Foundation.h>
 
 int main(void) {
-
-	NSFileManager *fileManager;
-
-	fileManager = [NSFileManager defaultManager];
+	NSFileManager *fileManager = [NSFileManager defaultManager];
 	NSData *fileData = fileManager contentsAtPath:@"read_file.m";
 	NSString* dataString = [NSString stringWithUTF8String:[fileData bytes]];
 
