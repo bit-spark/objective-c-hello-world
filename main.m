ae5414f0f06d0c3d5ee8f01d2e4a8f7dff150c7c
James Ridgway <james.ridgway@thefloow.com>
2014-01-21 08:30:43 +0000

    Fixes 2

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ int main(void)
 
 	NSDate *now = [NSDate date];
 	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init]; 
-	[dateFormatter setFormat:@"dd-MM-yyyy"]
+	[dateFormatter setFormat:@"dd-MM-yyyy"];
 	NSString *dateString = [dateFormatter stringFromDate:date];
 	
 	NSLog(@"Today's date is %@", dateString);
