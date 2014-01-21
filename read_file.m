e7f26ce45ec9e423dfe412b09b5a0845fc4c404b
James Ridgway <james.ridgway@thefloow.com>
2014-01-21 08:42:04 +0000

    fixed file

--- read_file.m
+++ read_file.m
@@ -2,10 +2,10 @@
 
 int main(void) {
 	NSFileManager *fileManager = [NSFileManager defaultManager];
-	NSData *fileData = fileManager contentsAtPath:@"read_file.m";
+	NSData *fileData = [fileManager contentsAtPath:@"read_file.m"];
 	NSString* dataString = [NSString stringWithUTF8String:[fileData bytes]];
 
 	NSLog(@"File contents = %@", dataString);
-	
+
 	return 0;
 }
