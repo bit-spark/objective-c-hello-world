71213a877a61fdbfdb0aadf315aa67a764530ab5
James Ridgway <james.ridgway@thefloow.com>
2014-01-21 08:30:27 +0000

    fixes 1

--- main.m
+++ main.m
@@ -1,10 +1,10 @@
-#import <stdio.h>
+#import <Foundation/Foundation.h>
 
 int main(void)
 {
 	NSLog(@"Hello world!");
 
-	NSDate now = [NSDate date];
+	NSDate *now = [NSDate date];
 	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init]; 
 	[dateFormatter setFormat:@"dd-MM-yyyy"]
 	NSString *dateString = [dateFormatter stringFromDate:date];
