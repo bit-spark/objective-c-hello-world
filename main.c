c516546f5491f90c8c4b546b0b7685bb801692b1
James Ridgway <james.ridgway@thefloow.com>
2014-01-21 08:26:45 +0000

    main.c

--- main.c
+++ main.c
@@ -3,5 +3,13 @@
 int main(void)
 {
 	NSLog(@"Hello world!");
+
+	NSDate now = [NSDate date];
+	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init]; 
+	[dateFormatter setFormat:@"dd-MM-yyyy"]
+	NSString *dateString = [dateFormatter stringFromDate:date];
+	
+	NSLog(@"Today's date is %@", dateString);
+
 	return 0;
 }
